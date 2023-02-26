ce8c67a295e42c9ad3111ca714fd0315b32aec29
AUNGMYATLWIN <zack.cloud7@gmail.com>
2023-02-26 21:42:14 +0630

    added inputStyles in TextInput

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,4 +1,4 @@
-import { Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 
 function Input({label, style, textInputConfig}){
@@ -8,7 +8,7 @@ function Input({label, style, textInputConfig}){
     }
     return <View style={[styles.inputContainer, style]}>
         <Text style={styles.label}>{label}</Text>
-        <TextInput {...textInputConfig} style={styles.inputMultiLine}/>
+        <TextInput {...textInputConfig} style={inputStyles}/>
     </View>
 
 }
@@ -28,10 +28,10 @@ const styles=StyleSheet.create({
     },
     input:{
         backgroundColor:GlobalStyles.colors.primary100,
+        color:GlobalStyles.colors.primary700,
         padding:6,
         borderRadius:6,
         fontSize:18,
-        color:GlobalStyles.colors.primary700
     },
     inputMultiLine:{
         minHeight:100,
