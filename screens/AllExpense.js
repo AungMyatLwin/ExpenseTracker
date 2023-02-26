8e9dcf046136c5cc27f86f7f4f4ca2d7be87ff54
AUNGMYATLWIN <zack.cloud7@gmail.com>
2023-02-26 21:57:23 +0630

    setted flex to 1

--- screens/AllExpense.js
+++ screens/AllExpense.js
@@ -1,14 +1,19 @@
 import { useContext } from "react";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 
 
 function AllExpense(){
     const expensesCtx=useContext(ExpensesContext);
-    return <View>
+    return <View style={styles.viewContainer}>
         <ExpensesOutput expensesPeriod="Total" expenses={expensesCtx.expenses} fallbackText="No register expenses found"/>
     </View>
 }
 
 export default AllExpense;
+const styles=StyleSheet.create({
+    viewContainer:{
+        flex:1
+    }
+});
