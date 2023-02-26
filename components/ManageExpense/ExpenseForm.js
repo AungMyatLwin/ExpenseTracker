45289f6a0c7a711976a371799f731330da018c32
AUNGMYATLWIN <zack.cloud7@gmail.com>
2023-02-26 15:04:34 +0630

    added styles

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,4 +1,5 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 import Input from "./Input";
 
 function ExpenseForm(){
@@ -9,17 +10,36 @@ function ExpenseForm(){
         <Input label="Amount" textInputConfig={{
             keyboardType:"decimal-pad",
             onChangeText:amountChangeHandler,
-        }}/>
+        }} style={styles.inputContainer}/>
         <Input label="Date" textInputConfig={{
             onChangeText:()=>{},
             placeholder:"YYYY-MM-DD",
             maxLength:10,
-        }}/>
+        }} style={styles.label}/>
         <Input label="Description"textInputConfig={{
             multiLine: true,
-        }}/>
+        }} style={styles.input}/>
     </View>
 
 }
 
 export default ExpenseForm;
+
+const styles=StyleSheet.create({
+    inputContainer:{
+        marginHorizontal:4,
+        marginVertical:8,
+    },
+    label:{
+        fontSize:12,
+        color:GlobalStyles.colors.primary100,
+        marginBottom:4
+    },
+    input:{
+        backgroundColor:GlobalStyles.colors.primary100,
+        padding:6,
+        borderRadius:6,
+        fontSize:18,
+        color:GlobalStyles.colors.primary700
+    }
+});
